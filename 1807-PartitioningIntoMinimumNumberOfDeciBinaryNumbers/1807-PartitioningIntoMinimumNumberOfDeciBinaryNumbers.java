a07f47db21ca1392620dd93f5fc8bc5d0dd39ed4
vikramhankare <30934218+vikramhankare@users.noreply.github.com>
2026-06-07 18:30:39 +0530

    Create 1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers

---
+++ 1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers/1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,10 @@
+// Last updated: 6/7/2026, 6:30:38 PM
+class Solution {
+    public int minPartitions(String n) {
+        int ans = 0;
+        for (int i = 0; i < n.length(); ++i) {
+            ans = Math.max(ans, n.charAt(i) - '0');
+        }
+        return ans;
+    }
+}
